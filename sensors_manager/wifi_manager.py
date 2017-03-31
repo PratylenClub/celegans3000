8b48594c0a70201009f4f2991d7b37f8466c4560
SergioPeignier <sergio.peignier.zapata@gmail.com>
2017-03-31 23:04:19 +0000

    first working version of wifi_junky

--- sensors_manager/wifi_manager.py
+++ sensors_manager/wifi_manager.py
@@ -1,15 +1,14 @@
 import subprocess
 import time
 
-def get_wifi_quality(interface = "wlan0"):
-    cmd = subprocess.Popen('iwconfig %s' % interface, shell=True, stdout=subprocess.PIPE)
-    for line in cmd.stdout:
-        if 'Link Quality' in line:
-            return float(line.split(" ")[-2].split("=")[-1])
-        elif 'Not-Associated' in line:
-            return None
-    return None
-
+def get_wifi_quality(interface = "wlan0", feature_name = "Link Quality", sep = "="):
+    cmd = subprocess.Popen('iwconfig '+interface+' | grep \"'+ feature_name+'\"', shell=True, stdout=subprocess.PIPE)
+    line = cmd.stdout.read()
+    feature_value = line.split(feature_name)[1].split()[0].split(sep)[1].split("/")[0]
+    return eval(feature_value)
+"""
 for i in range(100):
 	print get_wifi_quality()
 	time.sleep(0.5)
+
+"""
--- wifi_junky.py
+++ wifi_junky.py
@@ -1,5 +1,5 @@
 import time
-import robot_manager.robot as robot
+import robot_manager.robot as Robot
 import sensors_manager.wifi_manager as wifi_manager
 import random
 LEFT_TRIM   = 0
@@ -10,40 +10,51 @@ MIN_ANGULAR_SPEED = 0
 MAX_LINEAR_SPEED = 255
 MIN_LINEAR_SPEED = 0
 
-MOVEMENT_TIME = 1
+MOVEMENT_TIME = 1.5
 
-def random_movement(self):
-	angular_speed = random.randint(-MAX_ANGULAR_SPEED-1,MAX_ANGULAR_SPEED)
-	angular_time = MOVEMENT_TIME
-	linear_speed = random.randint(-MAX_ANGULAR_SPEED-1,MAX_ANGULAR_SPEED)
-	linear_time = MOVEMENT_TIME	
+def random_movement():
+	if random.random() < 0.3:
+		angular_speed = random.randint(-MAX_ANGULAR_SPEED-1,MAX_ANGULAR_SPEED)
+		angular_time = MOVEMENT_TIME
+		linear_speed = random.randint(-MAX_ANGULAR_SPEED-1,MAX_ANGULAR_SPEED)
+		linear_time = MOVEMENT_TIME	
+	else:
+		angular_speed = 0
+                angular_time = MOVEMENT_TIME
+                linear_speed = random.randint(-MAX_ANGULAR_SPEED-1,MAX_ANGULAR_SPEED)
+                linear_time = MOVEMENT_TIME
 	return angular_speed, angular_time, linear_speed, linear_time
 
-def reverse_movement(self, angular_speed, angular_time, linear_speed, linear_time):
+def reverse_movement(angular_speed, angular_time, linear_speed, linear_time):
 	return -1*angular_speed, angular_time, -1* linear_speed, linear_time
 
-def execute_movement(self, robot, angular_speed, angular_time, linear_speed, linear_time):
+def execute_movement(robot, angular_speed, angular_time, linear_speed, linear_time):
+        print angular_speed, angular_time, linear_speed, linear_time
 	if angular_speed < 0:
 		robot.left(-1*angular_speed, angular_time)
 	else:
-		robot.write(angular_speed, angular_time)
+		robot.right(angular_speed, angular_time)
 	if linear_speed < 0:
 		robot.backward(-1*linear_speed, linear_time)
 	else:
 		robot.forward(linear_speed, linear_time)
 
 
-def search_wifi(self, robot, nb_iterations=20):
+def search_wifi(robot, nb_iterations=20):
 	for i in xrange(nb_iterations):
-		previous_wifi_signal = wifi_manager.get_wifi_quality()
+                print "_____"
+                previous_wifi_signal = wifi_manager.get_wifi_quality()
+                print previous_wifi_signal
 		random_step = random_movement()
 		execute_movement(robot, *random_step)
 		new_wifi_signal = wifi_manager.get_wifi_quality()
+                print new_wifi_signal
 		if new_wifi_signal < previous_wifi_signal:
 			reverse_random_step = reverse_movement(*random_step)
 			execute_movement(robot, *reverse_random_step)
-
+		time.sleep(2)
 
 
 robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 search_wifi(robot)	
+#robot.right(100,2)
