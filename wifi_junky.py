be8faa42bd12b9d3bb3c182ed84b8396d8ebfa7e
SergioPeignier <sergio.peignier.zapata@gmail.com>
2017-04-03 00:04:28 +0200

    indentation problems with wifi_junky

--- wifi_junky.py
+++ wifi_junky.py
@@ -20,26 +20,25 @@ def random_movement():
 		linear_time = MOVEMENT_TIME	
 	else:
 		angular_speed = 0
-                angular_time = MOVEMENT_TIME
-                linear_speed = random.randint(-MAX_LINEAR_SPEED-1,MAX_LINEAR_SPEED)
-                linear_time = MOVEMENT_TIME
+		angular_time = MOVEMENT_TIME
+		linear_speed = random.randint(-MAX_LINEAR_SPEED-1,MAX_LINEAR_SPEED)
+		linear_time = MOVEMENT_TIME
 	return angular_speed, angular_time, linear_speed, linear_time
 
 def execute_reverse_movement(robot, angular_speed, angular_time, linear_speed, linear_time):
-        print "entering reverse move"
 	linear_speed *= -1
 	angular_speed *= -1
 	if linear_speed < 0:
-                robot.backward(-1*linear_speed, linear_time)
-        else:
-                robot.forward(linear_speed, linear_time)
+		robot.backward(-1*linear_speed, linear_time)
+	else:
+		robot.forward(linear_speed, linear_time)
 	if angular_speed < 0:
-                robot.left(-1*angular_speed, angular_time)
-        else:
-                robot.right(angular_speed, angular_time)
+		robot.left(-1*angular_speed, angular_time)
+	else:
+		robot.right(angular_speed, angular_time)
 
 def execute_movement(robot, angular_speed, angular_time, linear_speed, linear_time):
-        print angular_speed, angular_time, linear_speed, linear_time
+	print angular_speed, angular_time, linear_speed, linear_time
 	if angular_speed < 0:
 		robot.left(-1*angular_speed, angular_time)
 	else:
@@ -52,13 +51,10 @@ def execute_movement(robot, angular_speed, angular_time, linear_speed, linear_ti
 
 def search_wifi(robot, nb_iterations=20):
 	for i in xrange(nb_iterations):
-                print "_____"
-                previous_wifi_signal = wifi_manager.get_wifi_quality()
-                print previous_wifi_signal
+		previous_wifi_signal = wifi_manager.get_wifi_quality()
 		random_step = random_movement()
 		execute_movement(robot, *random_step)
 		new_wifi_signal = wifi_manager.get_wifi_quality()
-                print new_wifi_signal
 		if new_wifi_signal < previous_wifi_signal:
 			execute_reverse_movement(robot, *random_step)
 		time.sleep(2)
