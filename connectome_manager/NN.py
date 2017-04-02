4396b8d4990d4bd41f50d0b258180bd8fc2f3a97
SergioPeignier <sergio.peignier.zapata@gmail.com>
2017-04-02 10:07:54 +0200

    neural network model class

--- connectome_manager/NN.py
+++ connectome_manager/NN.py
@@ -1,2 +1,8 @@
-import pandas
-import numpy
+import pickle as p
+
+class NN:
+    def __init__(self,model_file):
+        model = p.load(open(model_file,"rb"))
+        self.Neural_Network = model["Neural_Network"]
+        self.Cells_state = model["Cells_state"]
+        
