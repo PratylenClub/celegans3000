ad7ee933a69c66a40d0ea3cb4ed204082f1a6c83
SergioPeignier <sergio.peignier.zapata@gmail.com>
2017-04-09 17:44:17 +0200

    erase maximal and minimal speed boundaries

--- CElegans3000-client.py
+++ CElegans3000-client.py
@@ -12,13 +12,14 @@ TCP_IP = '192.168.43.156'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 END_TASK_SIGNAL = "END_TASK"
-NB_TRIALS_ULTRASOUND = 4
-DELTA_TIME_ULTRASOUND = 0.1
+NB_TRIALS_ULTRASOUND = 3
+DELTA_TIME_ULTRASOUND = 0.2
 def print_me(*a):
 	print a
 
 class Body:
 	def __init__(self,tcp_id=TCP_IP,tcp_port=TCP_PORT):
+		self.energy = 100
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.connect((tcp_id, tcp_port))
 		#self.motor_actions = {'MOTOR_RIGHT':print_me, 'MOTOR_LEFT':print_me}
@@ -27,34 +28,31 @@ class Body:
 
 	def run_right_motor(self,weight,signal):
 		angular_speed = signal*weight*20
-		if angular_speed>200: angular_speed = 200
-		if angular_speed<80: angular_speed = 80
 		angular_time=0.5
 		self.body.only_right(int(angular_speed), angular_time)
+		self.energy -= angular_time*angular_speed
 
 	def run_left_motor(self,weight,signal):
 		angular_speed = signal*weight*20
-		if angular_speed>200: angular_speed = 200
-		if angular_speed<80: angular_speed = 80
 		angular_time=0.5
+		self.energy -= angular_time*angular_speed
 		self.body.only_left(int(angular_speed), angular_time)
 
 	def run_motors(self,weight_right,weight_left):
 		print "RUN MOTORS",weight_right,weight_left
-		angular_time=3
+		angular_time=1
 		angular_speed_right = weight_right
 		angular_speed_left = weight_left
-		if angular_speed_left>250: angular_speed_left = 250
-		if angular_speed_left<40: angular_speed_left = 40
-		if angular_speed_right>250: angular_speed_right = 250
-		if angular_speed_right<40: angular_speed_right = 40
-		self.body.run_motors_forward(angular_speed_right, angular_speed_left,angular_time)
+		self.energy -= angular_time*(angular_speed_right + angular_speed_left)
+		self.body.run_motors_forward(int(angular_speed_right), int(angular_speed_left),angular_time)
 
 	def get_sensory_signals(self):
-		#wifi_signal = wifi_manager.get_wifi_quality()
+		wifi_signal = wifi_manager.get_wifi_quality()
 		ultra_sound_signal = ultra_sound.return_distance_to_obstacle(NB_TRIALS_ULTRASOUND ,DELTA_TIME_ULTRASOUND )
 		print "ULTRA SOUND",ultra_sound_signal
-		return {"ULTRA_SOUND":ultra_sound_signal}
+		print "WIFI_SIGNAL",wifi_signal
+		print "FOOD_LEVEL",self.energy
+		return {"ULTRA_SOUND":ultra_sound_signal, "WIFI_SIGNAL":wifi_signal}#, "FOOD_LEVEL":self.energy}
 
 	def run(self):
 		while 1:
